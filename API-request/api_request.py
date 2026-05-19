f3872b84e643603982fb6982266bb811d830fbf8
vNair <vishnusn@outlook.com>
2026-05-18 19:28:33 -0500

    onto Integrating Airflow

--- API-request/api_request.py
+++ API-request/api_request.py
@@ -1,5 +1,8 @@
 import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 api_key = os.environ["WEATHER_API_KEY"]
 api_url = f"https://api.weatherstack.com/current?access_key={api_key}&query=New York"
@@ -16,7 +19,7 @@ def fetch_data():
         raise
 
 
-#fetch_data()
 
+# This just exists for now because we dont want to keep calling the API hitting rate limits
 def mock_fetch_data():
     return {'request': {'type': 'City', 'query': 'New York, United States of America', 'language': 'en', 'unit': 'm'}, 'location': {'name': 'New York', 'country': 'United States of America', 'region': 'New York', 'lat': '40.714', 'lon': '-74.006', 'timezone_id': 'America/New_York', 'localtime': '2026-05-18 16:45', 'localtime_epoch': 1779122700, 'utc_offset': '-4.0'}, 'current': {'observation_time': '08:45 PM', 'temperature': 28, 'weather_code': 113, 'weather_icons': ['https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0001_sunny.png'], 'weather_descriptions': ['Sunny'], 'astro': {'sunrise': '05:36 AM', 'sunset': '08:09 PM', 'moonrise': '06:41 AM', 'moonset': '11:00 PM', 'moon_phase': 'Waxing Crescent', 'moon_illumination': 2}, 'air_quality': {'co': '187.85', 'no2': '35.85', 'o3': '89', 'so2': '2.65', 'pm2_5': '18.15', 'pm10': '20.55', 'us-epa-index': '2', 'gb-defra-index': '2'}, 'wind_speed': 22, 'wind_degree': 158, 'wind_dir': 'SSE', 'pressure': 1021, 'precip': 0, 'humidity': 51, 'cloudcover': 0, 'feelslike': 28, 'uv_index': 4, 'visibility': 16, 'is_day': 'yes'}}
--- API-request/insert_records.py
+++ API-request/insert_records.py
@@ -46,22 +46,49 @@ conn = connect_to_db()
 create_table(conn)
 
 
-# def insert_records(conn, data):
-#     print("Inserting weather data into the database...")
-#     try: 
-#         cursor = conn.cursor()
-#         cursor.execute("""
-#             INSERT INTO dev.raw_weather_data(
-#                 city TEXT,
-#                 temperature FLOAT,
-#                 weather_descriptions TEXT, 
-#                 wind_speed FLOAT,
-#                 time TIMESTAMP, 
-#                 inserted_at TIMESTAMP DEFAULT NOW(), 
-#                 utc_offset TEXT  
-#             )
-#         """)
-#     except:
+def insert_records(conn, data):
+    print("Inserting weather data into the database...")
+    try: 
+        weather = data['current']
+        location = data['location']
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO dev.raw_weather_data(
+                city,
+                temperature,
+                weather_descriptions, 
+                wind_speed,
+                time , 
+                inserted_at, 
+                utc_offset   
+            ) VALUES (%s, %s, %s, %s, %s, NOW(), %s)
+        """,(
+            location['name'], 
+            weather['temperature'],
+            weather['weather_descriptions'][0],
+            weather['wind_speed'],
+            location['localtime'], 
+            location['utc_offset']
+        ))
+        conn.commit()
+        print("Data successfully inserted")
+    except psycopg2.Error as e:
+        print(f"Error inserting data into the database: {e}")
+        raise
 
-    
-        
+def main():
+    try:
+        data = mock_fetch_data()
+        conn = connect_to_db()
+        create_table(conn)
+        insert_records(conn, data)
+    except Exception as e:
+        print(f"An error occurred during execution: {e}")
+    finally:
+        if 'conn' in locals():
+            conn.close()
+            print("Database connection closed.")
+
+
+
+main() # call the main function
